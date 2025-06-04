ae20272bc9fd7422570ef4beb4016c9bf02a6dd0
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-06-04 12:29:33 -0300

    Melhorando a segurança da rota que busca todos os usuários.

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -5,7 +5,7 @@ const authRouter = express.Router()
 
 authRouter.post('/register', userController.register)
 authRouter.post('/login', userController.login)
-authRouter.get('/users', authMiddleware.authenticate, authMiddleware.onlyAdmin, userController.showAll)
+authRouter.get('/users', authMiddleware.authenticate, authMiddleware.safeRouter, authMiddleware.onlyAdmin, userController.showAll)
 authRouter.get('/users/:id', authMiddleware.authenticate, authMiddleware.safeRouter, userController.userById)
 authRouter.put('/update/:id', authMiddleware.authenticate, authMiddleware.safeRouter, userController.update)
 authRouter.delete('/delete/:id', authMiddleware.authenticate, authMiddleware.safeRouter, authMiddleware.onlyAdmin, userController.delete)
