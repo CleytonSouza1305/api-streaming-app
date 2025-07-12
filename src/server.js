47aaa72d087e8ea85831059c63785897112c0bd9
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-12 04:16:50 -0300

    Tentando usar o cors no projeto ainda

--- src/server.js
+++ src/server.js
@@ -7,7 +7,12 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use('/auth', authRouter)
 
