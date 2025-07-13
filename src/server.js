f86b3576d58719b96ff5449280ae5737f4bdaa73
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-13 02:28:54 -0300

    Arrumando arquivo do app

--- src/server.js
+++ src/server.js
@@ -8,16 +8,11 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+app.use(cors())
 app.use('/auth', authRouter)
 
 app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Servidor rodando em:\nhttp://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`))
 
