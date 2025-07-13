2c3b0158fbbdfdfec87bf4eea3ac961393b35028
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-13 02:32:42 -0300

    Arrumando o cors

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const errorMiddleware = require('./middleware/error-middleware')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
-
 app.use(cors())
+
+app.use(express.json())
 app.use('/auth', authRouter)
 
 app.use(errorMiddleware)
