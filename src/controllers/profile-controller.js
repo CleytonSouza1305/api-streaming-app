a407fe59c6b8b7ddda392b6b7abb7578babd74e9
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-15 15:52:04 -0300

    Criando o controller que lida com a requisição de criar um novo perfil.

--- src/controllers/profile-controller.js
+++ src/controllers/profile-controller.js
@@ -1,4 +1,6 @@
 const HttpError = require("../error/Error")
+const Profile = require("../models/Profile")
+const uuid = require('uuid').v4
 
 module.exports = {
   allProfiles(req, res, next) {
@@ -8,10 +10,45 @@ module.exports = {
       if (!req.user.profiles) {
         throw new HttpError(400, 'Perfis de usuário não encontrado.')
       }
-      
+
       res.json(user.profiles)
     } catch (e) {
       next(e)
     }
+  },
+
+  async createProfile(req, res, next) {
+    try {
+      const profiles = req.user.profiles
+
+      if (!profiles) {
+        throw new HttpError(400, 'Perfis de usuário não encontrado.')
+      }
+
+      const dbDataProfiles = await Profile.allProfiles(req.user.id)
+      if (dbDataProfiles.length >= 5) {
+        console.log('igual a 5')
+        throw new HttpError(400, 'Limite máximo de 5 perfis.')
+      }
+
+      const { profileName, isKid = false, profilePin = null} = req.body
+
+      if (!profileName) {
+        throw new HttpError(400, 'O nome de perfil é obrigatório.')
+      }
+
+      if (profilePin && profilePin.length !== 4) {
+        throw new HttpError(400, 'A senha deve conter 4 números')
+      }
+
+      const profileId = uuid()
+
+      const newProfile = await Profile.createProfile(profileId, req.user.id, profileName, isKid, profilePin)
+
+      res.json(newProfile)
+
+    } catch (e) {
+      next(e)
+    } 
   }
 }
