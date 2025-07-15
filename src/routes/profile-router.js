1ade84fcc2675230be8da8e24d18b2fb8ddd567d
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-15 15:53:17 -0300

    Adicionando a rota post para criar um novo perfil

--- src/routes/profile-router.js
+++ src/routes/profile-router.js
@@ -4,5 +4,6 @@ const profileController = require('../controllers/profile-controller')
 const profileRouter = express.Router()
 
 profileRouter.get('/profiles', authMiddleware.authenticate, profileController.allProfiles)
+profileRouter.post('/profile/create', authMiddleware.authenticate, profileController.createProfile)
 
 module.exports = profileRouter
