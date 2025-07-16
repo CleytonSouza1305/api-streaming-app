99cd9cfb0a7952eb49efe3996a87815a02a7dff6
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-16 14:55:23 -0300

    Criando a rota put para atualizar os campos

--- src/routes/profile-router.js
+++ src/routes/profile-router.js
@@ -4,6 +4,7 @@ const profileController = require('../controllers/profile-controller')
 const profileRouter = express.Router()
 
 profileRouter.get('/profiles', authMiddleware.authenticate, profileController.allProfiles)
-profileRouter.post('/profile/create', authMiddleware.authenticate, profileController.createProfile)
+profileRouter.post('/profiles', authMiddleware.authenticate, profileController.createProfile)
+profileRouter.put('/profiles/:id', authMiddleware.authenticate, profileController.update)
 
 module.exports = profileRouter
