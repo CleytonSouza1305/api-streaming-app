5acd3ff0602ef4420a21bf7d504358689c20599e
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2025-07-17 19:18:09 -0300

    Adicionando rota get que busca um perfil por id, e rota delete

--- src/routes/profile-router.js
+++ src/routes/profile-router.js
@@ -5,6 +5,8 @@ const profileRouter = express.Router()
 
 profileRouter.get('/profiles', authMiddleware.authenticate, profileController.allProfiles)
 profileRouter.post('/profiles', authMiddleware.authenticate, profileController.createProfile)
+profileRouter.get('/profiles/:id', authMiddleware.authenticate, profileController.showProfile)
 profileRouter.put('/profiles/:id', authMiddleware.authenticate, profileController.update)
+profileRouter.delete('/profiles/:id', authMiddleware.authenticate, profileController.delete)
 
 module.exports = profileRouter
